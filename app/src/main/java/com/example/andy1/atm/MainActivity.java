6938e08f343e2e1df30cc2877e3e4a0e91739ce0
andy0521 <andy132888@gmail.com>
2018-10-27 15:41:11 +0800

    20181027 ATM

--- app/src/main/java/com/example/andy1/atm/MainActivity.java
+++ app/src/main/java/com/example/andy1/atm/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.andy1.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public  static final  int RC_LOGIN=1;
+    boolean login =false;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!login){
+        Intent intent = new Intent(this, LoginActivity.class);
+//        startActivity(intent);
+            startActivityForResult(intent,RC_LOGIN);
+        }
     }
 }
