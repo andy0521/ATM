644ad0332b711184b881efcd821cd0ddfa880d7e
andy0521 <2drxxlox>
2018-11-13 15:23:58 +0800

    20181113 storage id

--- app/src/main/java/com/example/andy1/atm/LoginActivity.java
+++ app/src/main/java/com/example/andy1/atm/LoginActivity.java
@@ -13,24 +13,33 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-    public void login(View v){
         EditText edUserid=findViewById(R.id.ed_userid);
-        EditText edPassword=findViewById(R.id.ed_password);
-        String uid =edUserid.getText().toString();
-        String pw =edPassword.getText().toString();
-        if (uid.equals("jack")&&pw.equals("1234")){//login ok
-            Toast.makeText(this,"登入成功",Toast.LENGTH_LONG).show();
-            getIntent().putExtra("LOGIN_USERID",uid);
-            getIntent().putExtra("LOGIN_PASSWD",pw);
-            setResult(RESULT_OK,getIntent());
+        String userid = getSharedPreferences("atm",MODE_PRIVATE)
+                .getString("USERID","");
+                edUserid.setText(userid);
+
+    }
+    public void login(View v) {
+        EditText edUserid = findViewById(R.id.ed_userid);
+        EditText edPassword = findViewById(R.id.ed_password);
+        String uid = edUserid.getText().toString();
+        String pw = edPassword.getText().toString();
+        if (uid.equals("jack") && pw.equals("1234")) {//login ok
+            Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
+            getIntent().putExtra("LOGIN_USERID", uid);
+            getIntent().putExtra("LOGIN_PASSWD", pw);
+            setResult(RESULT_OK, getIntent());
+            getSharedPreferences("atm",MODE_PRIVATE)
+                    .edit()
+                    .putString("USERID",uid)
+                    .apply();
 
             finish();
-        }else{
+        } else {
             new AlertDialog.Builder(this)
                     .setTitle("ATM")
                     .setMessage("登入失敗")
-                    .setPositiveButton("ok",null)
+                    .setPositiveButton("ok", null)
                     .show();
         }
 
