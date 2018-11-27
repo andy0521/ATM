901643919884acea542db52beee70bfc30176744
andy0521 <andy132888@gmail.com>
2018-11-27 16:05:43 +0800

    20181127

--- app/src/main/java/com/example/andy1/atm/MainActivity.java
+++ app/src/main/java/com/example/andy1/atm/MainActivity.java
@@ -4,7 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends BasicActivity {
     public  static final  int RC_LOGIN=1;
@@ -20,7 +25,15 @@ public class MainActivity extends BasicActivity {
 //        startActivity(intent);
             startActivityForResult(intent,RC_LOGIN);
         }
+        List<String> fruits = Arrays.asList("香蕉" ,"鳳梨","芭樂");
+        ArrayAdapter<String> adapter = new
+                ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
+        ListView listView =findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
+
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
